2cdfd066a0f8ae50ebdb08e5dd32d1c3f510d11a
ibecir <becir@bioptimizers.com>
2023-11-28 22:12:00 +0100

    Initial commit

---
+++ src/test/java/springcloudfunctions/SpringCloudFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloudfunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFunctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
